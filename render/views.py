f89336f312704b62fa313a4398d8538cbd3db15f
LainusSG <191011022@acatlan.tecnm.mx>
2023-11-06 10:57:08 -0600

    first commit

--- render/views.py
+++ render/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'render/index.html', {})
+
+def index2(request):
+    return render(request, 'render/index2.html', {})
--- servidorRaspberry/urls.py
+++ servidorRaspberry/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', include('projects.urls')),
-    path('home', include('render.urls')),
+    path ('API', include('projects.urls')),
+    path('', include('render.urls')),
 
 ]
