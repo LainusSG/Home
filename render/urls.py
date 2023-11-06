111ed0519a5328ba643a791008d7e75b9569022f
LainusSG <191011022@acatlan.tecnm.mx>
2023-11-06 11:06:56 -0600

    first commit

--- render/urls.py
+++ render/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home', views.index2, name='index2'),
 ]
--- render/views.py
+++ render/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'render/index.html', {})
-
-def index2(request):
-    return render(request, 'render/index2.html', {})
