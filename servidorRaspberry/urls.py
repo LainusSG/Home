7b8028fe040b998ed5fac44fa72065b62af4efdc
LainusSG <191011022@acatlan.tecnm.mx>
2023-11-06 10:15:35 -0600

    first commit

--- servidorRaspberry/urls.py
+++ servidorRaspberry/urls.py
@@ -22,8 +22,7 @@ from views.HomeView2 import HomeView2
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', include('projects.urls')),
-    path ('casita', include('projects2.urls')),
     path ('home', HomeView.home, name ='home'),
-    path ('lecturas', HomeView2.home, name ='home')
+    path ('lecturas', HomeView2.home, name ='home2')
 
 ]
